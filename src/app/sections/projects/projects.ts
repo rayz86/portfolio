d49aaa2a4b169860324a9cf2083f50081fbf9e8b
Rayyan Shaikh <rayyanshaikh1986@gmail.com>
2025-11-19 18:08:52 +0400

    opti webp

--- src/app/sections/projects/projects.ts
+++ src/app/sections/projects/projects.ts
@@ -11,25 +11,25 @@ import { NgFor } from '@angular/common';
 export class Projects {
   projects = [
     {
-      image: "wahcabs_port.png",
+      image: "wahcabs_port.webp",
       title: "Wah Cabs - Cab Booking Platform",
       description: "A reliable cab and coach booking website with an admin panel to manage prices, bookings, vehicle availability etc with a tracker panel to handle bookings.",
       buttonUrl: "https://www.wahcabs.com",
     },
     {
-      image: "wahcruise_port.png",
+      image: "wahcruise_port.webp",
       title: "Wah Cruises - Cruise Booking Platform",
       description: "A cruise and yacht booking website with an admin panel to manage prices, addons, food items etc with a tracker panel to handle bookings.",
       buttonUrl: "https://www.wahcruises.netlify.app",
     },
     {
-      image: "proline_port.png",
+      image: "proline_port.webp",
       title: "Proline Interior - Interior Design Company",
       description: "An interior design company website showcasing their projects and services. Designed to attract potential clients and provide company information.",
       buttonUrl: "https://www.prolineinterior.com",
     },
     {
-      image: "mahajan_port.png",
+      image: "mahajan_port.webp",
       title: "Mahajan Connect - Moqsue Members Management",
       description: "A membership management system for mosques to manage member details, ancestral history and fees tracking. Includes a fees panel for reporting.",
       buttonUrl: "",
